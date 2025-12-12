f5348638d162394e03dbb375059c14d8059f41f1
vivek <tiwarivivek23@gmail.com>
2025-12-12 15:14:11 +0300

    removed unused refs

--- services/service-entity-producer/src/test/java/com/vivekt/services/eps/Test2Test.java
+++ services/service-entity-producer/src/test/java/com/vivekt/services/eps/Test2Test.java
@@ -1,6 +1,6 @@
 package com.vivekt.services.eps;
 
-import com.vivekt.App;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +12,6 @@ public class Test2Test {
 
     @Test
     void test1(){
-        // Arrange and Act
-        App app = new App();
-        app.main(null);
 
         //Assert
         Assertions.assertThat(1).isBetween(0,2);
