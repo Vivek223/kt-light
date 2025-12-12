22c5e92610698c1eaabc1b82043f1bb69954dabb
vivek <tiwarivivek23@gmail.com>
2025-12-12 14:21:07 +0300

    added services directory and entity producer service

---
+++ services/service-entity-producer/src/main/java/com/vivekt/EntityProducerServiceApp.java
@@ -0,0 +1,7 @@
+package com.vivekt;
+
+public class EntityProducerServiceApp {
+    public static void main(String[] args) {
+        System.out.println("coming entity producer service soon..");
+    }
+}
